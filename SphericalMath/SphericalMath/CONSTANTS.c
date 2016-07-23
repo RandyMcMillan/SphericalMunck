8c0f851594476499440fd639199210f05d292815
Randy McMillan <randy.lee.mcmillan@gmail.com>
2016-07-22 23:05:43 -0400

    git add * ; uncrustify; git commit

---
+++ SphericalMath/SphericalMath/CONSTANTS.c
@@ -0,0 +1,9 @@
+//
+//  CONSTANTS.c
+//  SphericalMath
+//
+//  Created by randymcmillan on 7/22/16.
+//  Copyright © 2016 Randy McMillan. All rights reserved.
+//
+
+#include "CONSTANTS.h"
---
+++ SphericalMath/SphericalMath/CONSTANTS.h
@@ -0,0 +1,14 @@
+//
+//  CONSTANTS.h
+//  SphericalMath
+//
+//  Created by randymcmillan on 7/22/16.
+//  Copyright © 2016 Randy McMillan. All rights reserved.
+//
+
+#ifndef CONSTANTS_h
+#define CONSTANTS_h
+
+#include <stdio.h>
+
+#endif /* CONSTANTS_h */
