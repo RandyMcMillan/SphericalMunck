343de10c9f672fae4e406217a050162ae791e207
Randy McMillan <randy.lee.mcmillan@gmail.com>
2016-07-24 12:12:30 -0400

    git add * ; uncrustify; git commit

--- SphericalMath/SphericalMath/CONSTANTS.h
+++ SphericalMath/SphericalMath/CONSTANTS.h
@@ -1,10 +1,10 @@
-//
-//  CONSTANTS.h
-//  SphericalMath
-//
-//  Created by randymcmillan on 7/22/16.
-//  Copyright © 2016 Randy McMillan. All rights reserved.
-//
+/*
+ * CONSTANTS.h
+ * SphericalMath
+ *
+ * Created by randymcmillan on 7/22/16.
+ * Copyright © 2016 Randy McMillan. All rights reserved.
+ */
 
 #ifndef CONSTANTS_h
 #define CONSTANTS_h
@@ -13,14 +13,39 @@
 #include <math.h>
 
 
-#define PI_SQUARED M_PI*M_PI
+#define PI_SQUARED  M_PI * M_PI
 
-#define TRIPLE_PI 3*M_PI
-#define DOUBLE_PI 2*M_PI
-#define PI M_PI
-#define HALF_PI M_PI_2
-#define THIRD_PI M_PI/3
-#define QUARTER_PI M_PI_4
+#define TRIPLE_PI   3 * M_PI
+#define DOUBLE_PI   2 * M_PI
+#define PI          M_PI
+#define HALF_PI     M_PI_2
+#define THIRD_PI    M_PI / 3
+#define QUARTER_PI  M_PI_4
+
+#define INVERSE_PI 1/M_PI
+#define INVERSE_DBL_PI 1/(2*M_PI)
+
+#define RADIAN 360/DOUBLE_PI
+
+#define R RADIAN
+
+#define THIRD_RADIAN R/3
+#define HALF_RADIAN R/2
+#define THREE_HUNDRED_SIXTY 360
 
 
-#endif /* CONSTANTS_h */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif/* CONSTANTS_h */
