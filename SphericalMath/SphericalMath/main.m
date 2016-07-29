7649039b855766bf1c559bfbe9abee91c6422e75
Randy McMillan <randy.lee.mcmillan@gmail.com>
2016-07-28 20:40:46 -0400

    git add * ; uncrustify; git commit

--- SphericalMath/SphericalMath/main.m
+++ SphericalMath/SphericalMath/main.m
@@ -1,16 +1,13 @@
-//
-//  main.m
-//  SphericalMath
-//
-//  Created by Randy McMillan on 4/13/13.
-//  Copyright (c) 2013 Randy McMillan. All rights reserved.
-//
+/*
+  main.m
+  SphericalMath
+  Created by Randy McMillan on 4/13/13.
+  Copyright (c) 2013 Randy McMillan. All rights reserved.
+*/
 
 #import <Foundation/Foundation.h>
 #include <math.h>
 #include "munck.h"
-
-
 
 int main(int argc, const char *argv[])
 {
--- SphericalMath/SphericalMath/munck.c
+++ SphericalMath/SphericalMath/munck.c
@@ -1,5 +1,5 @@
 /*
-  CONSTANTS.c
+  munck.c
   SphericalMath
   Created by randymcmillan on 7/22/16.
   Copyright © 2016 Randy McMillan. All rights reserved.
